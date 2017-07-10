d596f6fa43f3a3f432c067e3936eb4376602e9f7
jakdwo <jakub.dwojak@hotmail.com>
2017-07-10 14:48:12 +0200

    Done selectors

--- src/main/resources/static/app/searching/searching-controller.js
+++ src/main/resources/static/app/searching/searching-controller.js
@@ -8,7 +8,6 @@
 
     function SearchingController(SendData) {
         var vm = this;
-        vm.hey = "Hello world ! ";
         vm.values = [];
         vm.result = [];
         vm.addFields = function () {
@@ -17,6 +16,9 @@
         vm.getValues = function(){
             for(var i=0; i<vm.values.length;i++)
                 console.log(vm.values[i]);
+            if(vm.values.length === 0)
+                alert("Nic nie wybrano!");
+            else
             SendData.query({ldapFilters: vm.values},onSuccess,onError);
         };
         vm.delete = function () {
