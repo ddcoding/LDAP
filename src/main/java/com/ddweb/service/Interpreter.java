4f4dd589dcbb8b5607b9143af67739f3ff70b088
kardro <karol_drozdowski@o2.pl>
2017-07-10 11:50:17 +0200

    Test to interpreter

--- src/main/java/com/ddweb/service/Interpreter.java
+++ src/main/java/com/ddweb/service/Interpreter.java
@@ -6,12 +6,21 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * Interpreter class for replace values from client side to pair which contains key,value. It's necessary to communicate with LDAP repository
+ */
 @Component
 public class Interpreter {
 
-    public String interpret(String s) {
-        switch (s) {
+    public Interpreter() {
+    }
+
+    /**
+     * @param value
+     * @return key for LDAP communication
+     */
+    public String interpret(String value) {
+        switch (value) {
             case "admin":
                 return "ou";
             case "user":
@@ -22,13 +31,18 @@ public class Interpreter {
                 return null;
         }
     }
-
-    public List<String> merge(List<String> strings){
+    /**
+    *   That method is merging values with keys using interpret method.
+     *   @param valuesList
+     *   @return newList which contains pairs of key and value. In first index of list, we have key and in the second we have value. newList[0] = key , newList[1] = value
+     *
+    */
+    public List<String> merge(List<String> valuesList) {
         List<String> newList = new ArrayList<>();
         String inter;
-        for(String s:strings){
+        for (String s : valuesList) {
             inter = interpret(s);
-            if(inter!=null) {
+            if (inter != null) {
                 newList.add(inter);
                 newList.add(s);
             }
