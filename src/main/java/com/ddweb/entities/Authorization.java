5201213b9c829d353598dd7c3c54a8a0c107628c
jakdwo <jakub.dwojak@hotmail.com>
2017-07-13 16:43:43 +0200

    entities

---
+++ src/main/java/com/ddweb/entities/Authorization.java
@@ -0,0 +1,61 @@
+package com.ddweb.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "authorization")
+public class Authorization implements Serializable {
+    public static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "auth_id")
+    private Long id;
+
+    @Column(name = "auth_name")
+    private String name;
+    @Column(name = "auth_description")
+    private String descirption;
+
+    public Authorization() {
+    }
+
+    public Authorization(String name, String descirption) {
+        this.name = name;
+        this.descirption = descirption;
+    }
+
+    @Override
+    public String toString() {
+        return "Authorization{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", descirption='" + descirption + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescirption() {
+        return descirption;
+    }
+
+    public void setDescirption(String descirption) {
+        this.descirption = descirption;
+    }
+}
